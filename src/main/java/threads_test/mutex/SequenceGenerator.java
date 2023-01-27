a148871f640661925ab371b12ab1463acef138c6
Marcos <marcos_fausto@yahoo.com.br>
2023-01-27 18:46:02 +0000

    added more tests

---
+++ src/main/java/threads_test/mutex/SequenceGenerator.java
@@ -0,0 +1,53 @@
+package threads_test.mutex;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SequenceGenerator {
+
+    private int currentValue = 0;
+
+    public synchronized int getNextSequence() {
+        currentValue = currentValue + 1;
+        return currentValue;
+    }
+
+    @Test
+    public void givenUnsafeSequenceGenerator_whenRaceCondition_thenUnexpectedBehavior() throws Exception {
+        int count = 1000000;
+        Set<Integer> uniqueSequences = getUniqueSequences(new SequenceGenerator(), count);
+        assertEquals(count, uniqueSequences.size());
+    }
+
+    private Set<Integer> getUniqueSequences(SequenceGenerator generator, int count) throws Exception {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        Set<Integer> uniqueSequences = new LinkedHashSet<>();
+        List<Future<Integer>> futures = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            futures.add(executor.submit(generator::getNextSequence));
+        }
+
+        for (Future<Integer> future : futures) {
+            uniqueSequences.add(future.get());
+        }
+
+        executor.awaitTermination(1, TimeUnit.SECONDS);
+        executor.shutdown();
+
+        return uniqueSequences;
+    }
+
+}
